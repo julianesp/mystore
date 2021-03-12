98228beb0908163d7ad0af36ed63ea7a1c754c1b
Julian España <julii1295@gmail.com>
2021-03-11 19:47:14 -0500

    Subiendo mi sitio a Github

---
+++ README.md
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+#Mi perfil
+=======
+#Mi sitio
+>>>>>>> cfbc1df0179e652793ad45ea09c3a2b7d750eb28
+
+Este sitio está diseñando para publicar temas de mi interés en tecnología.
---
+++ js/app.js
@@ -0,0 +1,114 @@
+particlesJS({
+    "particles": {
+        "number": {
+            // cantidad de circulos
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            /*color de la nieve*/
+            "value": "#fff"
+        },
+        "shape": {
+            //para type a image para colocar una imagen de animación    
+            "type": "circle",
+            "stroke": {
+                // color del borde de los circulos 
+                "width": 1,
+                "color": "#fff"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+            "image": {
+                "src": "noel.png",
+                "width": 500,
+                "height": 500
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 10,
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 10,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 100,
+            "color": "#fff",
+            "opacity": 0.4,
+            "width": 2
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 0.5
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 4,
+                "duration": 0.3,
+                "opacity": 1,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
